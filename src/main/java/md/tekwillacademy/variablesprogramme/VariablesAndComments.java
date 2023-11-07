edf6a08b70e8d0aae2e90e5d30ab2833f3fa6cbb
dimatana <dima.tanasciuc@gmail.com>
2023-11-07 12:12:47 +0200

    Added the program related to variables and comments

---
+++ src/main/java/md/tekwillacademy/variablesprogramme/VariablesAndComments.java
@@ -0,0 +1,45 @@
+package md.tekwillacademy.variablesprogramme;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+        //declare the variables
+
+        boolean isUsed;
+        boolean hasBattery;
+        boolean isAndroid;
+        boolean isStolen;
+        boolean is5G;
+
+        char condition;
+        byte numberOfSimCards;
+        byte randomAccesMemory;
+
+        short productionYear;
+
+        int numberOfOwners;
+        int id;
+
+        float operatingSistemVersion;
+
+        double price;
+
+        //Initialize an Iphone
+        isUsed = false;
+        hasBattery = true;
+        isAndroid = false;
+        isStolen = false;
+        is5G = true;
+
+        condition = 'A';
+        numberOfSimCards = 2;
+        randomAccesMemory = 4;
+        productionYear = 2023;
+        numberOfOwners = 0;
+        operatingSistemVersion = 16.4F;
+        price = 24342.3;
+
+        System.out.println("The phone is used " + isUsed);
+        System.out.println("The phone price is " + price);
+
+    }
+}
